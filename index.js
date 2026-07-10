56daafc0fb13077a1024c7271091cadc2ce96fdc
mirisrar <sardarzadaisrarrakhshani1@gmail.com>
2026-07-10 22:01:04 +0500

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const supabase = createClient(
 
 async function analyzeNews(title) {
   const response = await fetch(
-    `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=${process.env.GEMINI_API_KEY}`,
+    `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash-lite:generateContent`,
     {
       method: "POST",
       headers: {
