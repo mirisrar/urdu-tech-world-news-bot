ddcf0c39ade12b2a02ba347298472865e115d3b4
mirisrar <sardarzadaisrarrakhshani1@gmail.com>
2026-07-11 15:57:28 +0500

    Fix Gemini Model

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ async function analyzeNews(title) {
   console.log("KEY EXISTS:", !!process.env.GEMINI_API_KEY);
   console.log("KEY LENGTH:", process.env.GEMINI_API_KEY?.length);
   const response = await fetch(
-  `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash-lite:generateContent?key=${process.env.GEMINI_API_KEY}`,
+  `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=${process.env.GEMINI_API_KEY}`,
   {
       method: "POST",
       headers: {
