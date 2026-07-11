ae0382ad6f360b7bc40bb1c52fa73e8358a36ddc
mirisrar <sardarzadaisrarrakhshani1@gmail.com>
2026-07-11 15:50:02 +0500

    Update index.js

--- index.js
+++ index.js
@@ -12,8 +12,8 @@ async function analyzeNews(title) {
   console.log("KEY EXISTS:", !!process.env.GEMINI_API_KEY);
   console.log("KEY LENGTH:", process.env.GEMINI_API_KEY?.length);
   const response = await fetch(
-    `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash-lite:generateContent`,
-    {
+  `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash-lite:generateContent?key=${process.env.GEMINI_API_KEY}`,
+  {
       method: "POST",
       headers: {
         "Content-Type": "application/json"
